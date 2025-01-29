11655da9386be8b87585a0251b51f821c49dd101
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-01-29 21:13:34 +0300

    Adım 4 - 29.01.2025

--- AcunMedyaRestaurantly/Entities/Product.cs
+++ AcunMedyaRestaurantly/Entities/Product.cs
@@ -12,5 +12,6 @@ namespace AcunMedyaRestaurantly.Entities
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
