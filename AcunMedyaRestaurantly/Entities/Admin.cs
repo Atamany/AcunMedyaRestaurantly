d20031b68ef9fa1c125218613c2b2cf700e355c0
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-02-19 21:16:36 +0300

    19.02.2025 - Adım 5

--- AcunMedyaRestaurantly/Entities/Admin.cs
+++ AcunMedyaRestaurantly/Entities/Admin.cs
@@ -10,5 +10,9 @@ namespace AcunMedyaRestaurantly.Entities
         public int AdminId { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ImageUrl { get; set; }
+        public string Email { get; set; }
     }
 }
