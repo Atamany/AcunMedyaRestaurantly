eb6c4e569cf8e72bffad31fe46b197bea12cbee4
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-02-19 21:49:11 +0300

    19.02.2025 - Adım 7

--- AcunMedyaRestaurantly/Controllers/ProfilesController.cs
+++ AcunMedyaRestaurantly/Controllers/ProfilesController.cs
@@ -3,8 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Web;
 using System.Web.Mvc;
+using AcunMedyaRestaurantly.Entities;
 
 namespace AcunMedyaRestaurantly.Controllers
 {
@@ -17,6 +19,19 @@ namespace AcunMedyaRestaurantly.Controllers
         {
             var value = db.Admins.Find(Session["ID"]);
             return View(value);
+        }
+        [HttpPost]
+        public ActionResult Index(Admin p)
+        {
+            var value = db.Admins.Find(p.AdminId);
+            value.Username = p.Username;
+            value.Password = p.Password;
+            value.Name = p.Name;
+            value.Surname = p.Surname;
+            value.ImageUrl = p.ImageUrl;
+            value.Email = p.Email;
+            db.SaveChanges();
+            return RedirectToAction("Index", "Profiles");
         }
     }
 }
