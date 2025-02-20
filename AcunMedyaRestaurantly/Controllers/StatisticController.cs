a80b8669538fcae5eaf78ea5baec54e04c38cb4f
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-02-20 16:04:41 +0300

    Authorize'lar eklendi!

--- AcunMedyaRestaurantly/Controllers/StatisticController.cs
+++ AcunMedyaRestaurantly/Controllers/StatisticController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 
 namespace AcunMedyaRestaurantly.Controllers
 {
+    [Authorize]
     public class StatisticController : Controller
     {
         RestaurantlyContext db = new RestaurantlyContext();
